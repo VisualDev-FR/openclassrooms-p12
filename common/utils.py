ea3fb906d115072a528ae3b676d79714ae595854
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2023-11-01 16:19:13 +0100

    implement drop_none_from_dict in utils.py

--- common/utils.py
+++ common/utils.py
@@ -3,3 +3,7 @@ import re
 
 def email_is_valid(email: str):
     return re.fullmatch(r"\w+\.?\w+@\w+\.[a-z]+", email) is not None
+
+
+def drop_none_from_dict(data: dict) -> dict:
+    return dict([(key, value) for key, value in data.items() if value is not None])
