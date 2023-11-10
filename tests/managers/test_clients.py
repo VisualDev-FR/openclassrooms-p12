8aa988102bbdbc567d76310133ef1491e0dc619b
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2023-11-10 11:36:34 +0100

    update clients deletion tests

--- tests/managers/test_clients.py
+++ tests/managers/test_clients.py
@@ -2,9 +2,11 @@ import pytest
 import sqlalchemy
 from sqlalchemy.orm import Session
 
+from tests.conftest import login_as_accounting, login_as_sales, login_as_support
 from controller.managers import ClientsManager
 from models.clients import Client
-from tests.conftest import login_as_accounting, login_as_sales, login_as_support
+from models.contracts import Contract
+from models.events import Event
 
 
 DUMMY_CLIENT = {
@@ -30,7 +32,8 @@ def test_create_client_from_sales_employee(session: Session):
 
         assert created_client is not None
 
-        created_client = manager.get(Client.full_name == DUMMY_CLIENT["full_name"])[0]
+        created_client = manager.get(
+            Client.full_name == DUMMY_CLIENT["full_name"])[0]
 
         assert created_client.email == DUMMY_CLIENT["email"]
 
@@ -91,7 +94,31 @@ def test_get_client(session):
         assert client.id == 1
 
 
-def test_delete_client(session):
+def test_delete_client(session: Session):
+    """
+    Check that no user is allowed to delete a client.
+    """
+
+    manager = ClientsManager(session)
+
+    def count_all_contracts() -> int:
+        return len(session.scalars(sqlalchemy.select(Contract)).all())
+
+    def count_all_events() -> int:
+        return len(session.scalars(sqlalchemy.select(Event)).all())
+
+    with login_as_sales():
+
+        assert count_all_contracts() == 1
+        assert count_all_events() == 1
+
+        manager.delete(Client.email == CLIENT_EMAIL)
+
+        assert count_all_contracts() == 0
+        assert count_all_events() == 0
+
+
+def test_delete_client_from_unauthorized(session):
     """
     Check that no user is allowed to delete a client.
     """
@@ -102,9 +129,6 @@ def test_delete_client(session):
         manager.delete(Client.email == CLIENT_EMAIL)
 
     with login_as_accounting(), pytest.raises(PermissionError):
-        delete_client()
-
-    with login_as_sales(), pytest.raises(PermissionError):
         delete_client()
 
     with login_as_support(), pytest.raises(PermissionError):
